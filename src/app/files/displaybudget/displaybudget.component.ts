39f7ac52449ef4f572900585e0d5d09d291c8e7b
Oluwamaya <adeyemiawosemo@gmail.com>
2024-06-10 01:57:39 -0700

    starting to get fun

--- src/app/files/displaybudget/displaybudget.component.ts
+++ src/app/files/displaybudget/displaybudget.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
@@ -15,7 +15,10 @@ export class DisplaybudgetComponent {
    public itemForm : any = FormGroup
    public budgetInfo : any = {}
    public fetchItemList : any[] = [{}]
-  constructor(private actRoute: ActivatedRoute, private http: HttpClient ,private fb: FormBuilder){
+   public purchasedItemCount: number = 0;
+public unpurchasedItemCount: number = 0;
+
+  constructor(private actRoute: ActivatedRoute, private http: HttpClient ,private fb: FormBuilder, private cdr : ChangeDetectorRef){
     this.itemForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       quantity: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
@@ -41,9 +44,14 @@ export class DisplaybudgetComponent {
         console.log(res);
          this.budgetInfo = res.fetchBud
          this.fetchItemList = res.fetchItems
+         this.fetchItemList = res.fetchItems.map((item: any) => ({ ...item, checked: item.purchase }));
+         this.purchasedItemCount = this.fetchItemList.filter(item => item.purchase).length;
+this.unpurchasedItemCount = this.fetchItemList.filter(item => !item.purchase).length;
+
+         
          this.itemForm.patchValue({ userId: this.budgetInfo.userId });
-         console.log(this.budgetInfo);
-         console.log(this.fetchItemList);
+        //  console.log(this.budgetInfo);
+        //  console.log(this.fetchItemList);
          
          
       },(error)=>{
@@ -60,9 +68,10 @@ export class DisplaybudgetComponent {
       const formData = this.itemForm.value;
       console.log('Form Data:', formData);
 
-      this.http.post<any>('http://localhost:4444/itemList', formData).subscribe(
+      this.http.post<any>('http://localhost:4444/itemList', {formData}).subscribe(
         response => {
-          console.log('Response from backend:', response);
+          console.log(response);
+          this.fetchItemList = response.fetchItems
         },
         error => {
           console.error('Error:', error);
@@ -73,5 +82,31 @@ export class DisplaybudgetComponent {
     }
   }
 
+  
+  togglePurchased(item: any) {
+    const updatedItem = { ...item, purchase: !item.purchase };
+    
+    this.http.put(`http://localhost:4444/updateItem/${item._id}`, updatedItem).subscribe(
+      (response) => {
+       
+        // console.log(response);
+        const index = this.fetchItemList.findIndex(i => i._id === item._id);
+        if (index !== -1) {
+          this.fetchItemList[index].purchase = !item.purchase;
+          // Recalculate purchased and unpurchased item counts
+          this.purchasedItemCount = this.fetchItemList.filter(item => item.purchase).length;
+          this.unpurchasedItemCount = this.fetchItemList.filter(item => !item.purchase).length;
+        }
+        // console.log('New purchase state:', item.purchase);
+        this.cdr.detectChanges();
+        
+        
+      },
+      (error) => {
+        console.error('Error updating item', error);
+      }
+    );
+  }
+  
 
 }
